d173387503e06fcba4a5580262eb74f26c19c53c
Dyego Alekssander Maas <dyego.maas@gmail.com>
2020-07-28 20:12:23 -0300

    PING

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import unittest
+from unittest.mock import patch
 
 def fibonacci(pos):
+    if pos == 1:
+        return 1
     return 0
 
 class FibonacciTests(unittest.TestCase):
@@ -14,6 +17,14 @@ class FibonacciTests(unittest.TestCase):
         valor = fibonacci(pos=1)
 
         self.assertEqual(valor, 1)
+
+    @patch('builtins.input', return_value=[(2, 1), (3, 2), (4, 3), (5, 5), (6, 8), (7, 13)])
+    def test_o_proximo_valor_eh_a_soma_dos_dois_anteriores(self, retornos):
+        for par in retornos():
+            posicao, valor_esperado = par
+            valor = fibonacci(posicao)
+
+            self.assertEqual(valor, valor_esperado)
 
 if __name__ == '__main__':
     unittest.main()
